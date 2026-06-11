59e8a2b185ee2115a2ea22d7e9b1163a0d67beae
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-11 00:13:36 -0500

    style: js-beautify formatting (fetch-patch.js)

--- fetch-patch.js
+++ fetch-patch.js
@@ -1,23 +1,29 @@
-(()=>{
-  (()=>{
+(() => {
+  (() => {
     const _fetch = globalThis.fetch;
-    const globalThis.fetch = Object.setPrototypeOf(function fetch(...args){
-      try{
+    const globalThis.fetch = Object.setPrototypeOf(function fetch(...args) {
+      try {
         const $this = this;
-        args = args.map(x=>(x?.clone?.() ?? x));
-        return (async()=>{
-          try{
-            args = (await Promise.all(args)).map(x=>(x?.clone?.() ?? x));
-            return await $this.apply(_fetch,args);
-          }catch(e){
+        args = args.map(x => (x?.clone?.() ?? x));
+        return (async () => {
+          try {
+            args = (await Promise.all(args)).map(x => (x?.clone?.() ?? x));
+            return await $this.apply(_fetch, args);
+          } catch (e) {
             const msg = String(e);
-            return new Response(msg,{status:500,statusText:msg});
+            return new Response(msg, {
+              status: 500,
+              statusText: msg
+            });
           }
         })();
-      }catch(e){
+      } catch (e) {
         const msg = String(e);
-        return new Response(msg,{status:500,statusText:msg});
+        return new Response(msg, {
+          status: 500,
+          statusText: msg
+        });
       }
-    },_fetch);
+    }, _fetch);
   })();
 })();
